358f505598046a92ef39e6abd71e52c4ba8f6665
aylinataei <114648016+aylinataei@users.noreply.github.com>
2023-02-02 21:42:15 +0100

    change port var

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import app from './server.js';
 import * as dotenv from 'dotenv'
 
 dotenv.config();
-const port = process.env.port;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Started server at localhost: ${port}`);
